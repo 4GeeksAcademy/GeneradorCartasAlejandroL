489b756fb6bc7ddf4bc4166ae5f488aff6d5099f
alejandrolobatogil-dot <alejandrolobatogil@gmail.com>
2025-11-24 10:33:16 +0000

    GeneradorCartas1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let palo = ['spade', 'picas', 'heart', 'diamond'];
+let valor = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'rey', 'reina', 'jota', 'as'];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  function obtenerNumeroAleatorio(max){
+    return Math.floor(Math.random() * max);
+  }
+  function obtenerElementos(){
+    let carta = `${palo[obtenerNumeroAleatorio (palo.length)]}` + " " + `${valor[obtenerNumeroAleatorio (valor.length)]}`
+    document.getElementById("carta").innerHTML = carta;
+  }
+ obtenerElementos()
+}
+
+const reload = document.getElementById("reload");
+
+reload.addEventListener("click", (_) => {
+  location.reload();
+});
